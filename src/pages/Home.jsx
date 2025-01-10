74743b3d9cadd4340573c4ff55e003d46b14594e
zoroxzoro <Kaifalam9580@gmail.com>
2025-01-10 21:51:11 +0530

    frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,10 +58,7 @@ function Home() {
                   ></path>
                 </svg>
               </a>
-              <a
-                href="https://youtu.be/Q5LM985yUmQ"
-                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              >
+              <a className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 <svg
                   className="mr-2 -ml-1 w-5 h-5"
                   fill="currentColor"
@@ -70,7 +67,6 @@ function Home() {
                 >
                   <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
                 </svg>
-                Watch video
               </a>
             </div>
             <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
