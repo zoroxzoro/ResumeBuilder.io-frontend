50eb86d5ba2aca65e00d10704059a4068b390da4
zoroxzoro <Kaifalam9580@gmail.com>
2025-01-10 21:56:02 +0530

    frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,10 @@ function Home() {
               Effortlessly Craft a Standout Resume with Our AI-Powered Builder
             </p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-              <a className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+              <a
+                href="/dashboard"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              >
                 Get Started
                 <svg
                   className="ml-2 -mr-1 w-5 h-5"
